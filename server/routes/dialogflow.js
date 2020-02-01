207bbf954a1d6e262c90c480901b91de76a0436e
John Ahn <smileajw1004@gmail.com>
2020-02-01 21:33:57 +0900

    event route Added

--- server/routes/dialogflow.js
+++ server/routes/dialogflow.js
@@ -49,6 +49,33 @@ router.post('/textQuery', async (req, res) => {
 
 //Event Query Route
 
+router.post('/eventQuery', async (req, res) => {
+    //We need to send some information that comes from the client to Dialogflow API 
+    // The text query request.
+    const request = {
+        session: sessionPath,
+        queryInput: {
+            event: {
+                // The query to send to the dialogflow agent
+                name: req.body.event,
+                // The language used by the client (en-US)
+                languageCode: languageCode,
+            },
+        },
+    };
+
+    // Send request and log result
+    const responses = await sessionClient.detectIntent(request);
+    console.log('Detected intent');
+    const result = responses[0].queryResult;
+    console.log(`  Query: ${result.queryText}`);
+    console.log(`  Response: ${result.fulfillmentText}`);
+
+    res.send(result)
+})
+
+
+
 
 
 
